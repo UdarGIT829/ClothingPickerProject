77f600d4401e7ffe67c8a91a142dc9705e1d5c09
UdarGIT829 <viraatudar9@gmail.com>
2020-05-01 21:24:04 -0700

    Modified closestColor() so that it runs, but it's still not working correctly.

--- colors.py
+++ colors.py
@@ -11,6 +11,7 @@ fileImported = False
 def openFile(filename):# input string, return pillow image
     im = Image.open('gamut.png') # Input image file, source: https://github.com/jacksongabbard/Python-Color-Gamut-Generator/blob/master/gamut.png
     wheel = im.load() # use pillow library to read the image into an object
+    print("types: wheel= ", type(wheel),"|| im= ",type(im))
     center = (250,250) # set center to be half of width and height (500px, 500px)
     fileImported = True
     maxIterDistance = (int)((center[0]-pastFirstGray)/oneIterationDark)
@@ -25,29 +26,31 @@ def colorWheelImport(center, wheel,maxIterDistance): # using  pillow image, retu
         print(numerator,"*PI / ",denominator)
         for distance in range(1,maxIterDistance+1):
             newLocation = (((numerator*math.pi)/(denominator)), distance) # establish x,y coords based on radial coords
-            newColor = tuple(colorPicker(newLocation[0], newLocation[1],wheel)) # get (RGB) of pixel at location
+            newColor = (colorPicker(newLocation[0], newLocation[1],wheel)) # get (RGB) of pixel at location
             #print('\t',newColor)
-            accompanyingColors = accompanyingColorPicker(newLocation, wheel,maxIterDistance)
-            colorDictionary[newColor] = accompanyingColors
+            colorDictionary[newColor] = None
+        for color in colorDictionary:
+            accompanyingColors = accompanyingColorPicker(newLocation, wheel,maxIterDistance,colorDictionary)
+            colorDictionary[color] = accompanyingColors
     return colorDictionary
 
 #---DETERMINE ANALOGOUS COLORS-------------------------------------------------#
-def accompanyingColorPicker(originalColorLocation, wheel, maxIterDistance): # return format: [1Complement, 2-3 Triad, 4-8 Shades, 7-11Analogous]
+def accompanyingColorPicker(originalColorLocation, wheel, maxIterDistance,colorDictionary): # return format: [1Complement, 2-3 Triad, 4-8 Shades, 7-11Analogous]
     originalTheta = originalColorLocation[0]
     originalDistance = originalColorLocation[1]
     # Complement
     complementTheta = (originalTheta + math.pi)
     complement = colorPicker(complementTheta, originalDistance,wheel)
         # Round complement to existing colors
-    complement = closestColor(complement,wheel)
+    complement = closestColor(complement,colorDictionary)
     # Triad
     triad1Theta = (originalTheta + ((2*math.pi)/3))
     triad2Theta = (originalTheta + ((4*math.pi)/3))
     triad1 = colorPicker(triad1Theta,originalDistance,wheel)
     triad2 = colorPicker(triad2Theta,originalDistance,wheel)
         # Round triads to existing colors
-    triad1 = closestColor(triad1,wheel)
-    triad2 = closestColor(triad2,wheel)
+    triad1 = closestColor(triad1,colorDictionary)
+    triad2 = closestColor(triad2,colorDictionary)
     # Shades
     shadeDistList = []
     shadeList = []
@@ -69,7 +72,7 @@ def accompanyingColorPicker(originalColorLocation, wheel, maxIterDistance): # re
                 shadeDistList.append(originalDistance + counter)
     for dist in shadeDistList:
         # Round shades to closest existing colors
-        shade = closestColor(colorPicker(originalTheta, dist, wheel),wheel)
+        shade = closestColor(colorPicker(originalTheta, dist, wheel),colorDictionary)
         shadeList.append(shade)
     # Analogous colors
     analogousList = []
@@ -77,7 +80,7 @@ def accompanyingColorPicker(originalColorLocation, wheel, maxIterDistance): # re
         if coneAngleDiff != 0:
             newAnalogousAngle = originalTheta + coneAngleDiff * (math.pi/16)
         # Round analogous colors to closest existing colors
-            analogousColor = closestColor(colorPicker(newAnalogousAngle, originalDistance, wheel),wheel)
+            analogousColor = closestColor(colorPicker(newAnalogousAngle, originalDistance, wheel),colorDictionary)
             analogousList.append(analogousColor)
     return [complement,triad1,triad2,shadeList[0],shadeList[1],shadeList[2],shadeList[3],analogousList[0],analogousList[1],analogousList[2],analogousList[3]]
 
@@ -109,7 +112,8 @@ def closestColor(originalColor, colorDictionary):
             difference = sumDiff
             matchColor = existingColor
     if matchColor == originalColor:
-        print("No match found")
+        print(matchColor,": No match found")
     return matchColor
+
 initializeColors()
 
